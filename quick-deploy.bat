db02bf7fa9afcdb274799d9f137c58d4383ab593
Atul Raj <atul.dev13570@gmail.com>
2026-01-01 02:08:42 +0530

    Auto deploy

---
+++ quick-deploy.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Deploying...
+pnpm run deploy
+echo Done!
+pause
---
+++ setup-git.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Adding Git to PATH...
+setx PATH "%PATH%;C:\Program Files\Git\bin" /M
+echo Git added to PATH. Restart your terminal.
+pause
